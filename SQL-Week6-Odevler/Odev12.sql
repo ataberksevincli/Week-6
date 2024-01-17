8dd284f05bfec96abc177ab616500cbcf76c229b
Ataberk Sevinçli <sevincli.ataberk@hotmail.com>
2024-01-17 23:57:57 +0300

    sql-12 yapıldı

---
+++ SQL-Week6-Odevler/Odev12.sql
@@ -0,0 +1,20 @@
+SELECT COUNT(*) from film
+WHERE length > (
+SELECT ROUND (AVG(length))
+FROM film);
+
+SELECT COUNT(rental_rate) FROM film
+WHERE rental_rate =(
+SELECT MAX(rental_rate) FROM film
+);
+
+SELECT title,rental_rate,replacement_cost FROM film
+WHERE rental_rate = (
+SELECT MIN(rental_rate) FROM film
+)
+AND replacement_cost = (
+SELECT MIN(replacement_cost) FROM film
+);
+SELECT customer_id, COUNT(amount) AS payment_count FROM payment
+GROUP BY customer_id
+ORDER BY customer_id;
